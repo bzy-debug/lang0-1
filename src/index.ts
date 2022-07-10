1d1a71000cbcc774824d193a55263f84e6629d33
Zhiyuan Bao <bzy_sustech@foxmail.com>
2022-07-10 22:26:03 +0800

    use better practice

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
+// kind
 type Exp = Var | Fn | Ap
 
-type Var = string
+type Var = {
+  kind: "Var"
+  name: string
+}
 
 type Fn = {
+  kind: "Fn"
   var: Var
   body: Exp
 }
 
 type Ap = {
+  kind: "Ap"
   target: Exp
   arg: Exp
 }
@@ -19,17 +25,6 @@ type Value = {
 
 type Env = Map<string, Value>
 
-function isVar (exp: Exp): exp is Var {
-  return typeof exp === "string"
-}
-
-function isFn (exp: Exp): exp is Fn {
-  return (exp as Fn).var !== undefined && (exp as Fn).body !== undefined
-}
-
-function isAp (exp: Exp): exp is Ap {
-  return (exp as Ap).target !== undefined && (exp as Ap).arg !== undefined
-}
 function findValue (name: string, env: Env) {
   return env.get(name)
 }
@@ -44,16 +39,17 @@ function extend (env: Env, name: string, val: Value) {
 }
 
 function evaluate (exp: Exp, env: Env): Value {
-  if (isVar(exp)) return evaluateVar(exp as Var, env)
-  else if (isFn(exp)) return evaluateFn(exp as Fn, env)
-  else if (isAp(exp)) return evaluateAp(exp as Ap, env)
-  throw new Error("Unsupported exp")
+  switch (exp.kind) {
+    case "Var": return evaluateVar(exp, env)
+    case "Fn": return evaluateFn(exp, env)
+    case "Ap": return evaluateAp(exp, env)
+  }
 }
 
-function evaluateVar (variable: Var, env: Env): Value {
-  const value = findValue(variable, env)
+function evaluateVar (v: Var, env: Env): Value {
+  const value = findValue(v.name, env)
   if (value === undefined)
-    throw new Error(`undefined variable ${variable}`)
+    throw new Error(`undefined variable ${v.name}`)
   return value
 }
 
@@ -62,7 +58,7 @@ function evaluateFn (fn: Fn, env: Env): Value {
 }
 
 function apply (target: Value, arg: Value, env: Env): Value {
-  return evaluate(target.fnValue.body, extend(env, target.fnValue.var, arg))
+  return evaluate(target.fnValue.body, extend(env, target.fnValue.var.name, arg))
 }
 
 function evaluateAp (Ap: Ap, env: Env): Value {
@@ -72,8 +68,8 @@ function evaluateAp (Ap: Ap, env: Env): Value {
 }
 
 
-function union(set1: Set<Var>, set2: Set<Var>): Set<Var> {
-  const newSet: Set<Var> = new Set()
+function union(set1: Set<string>, set2: Set<string>): Set<string> {
+  const newSet: Set<string> = new Set()
 
   for (let item of set1)
     newSet.add(item)
@@ -84,8 +80,8 @@ function union(set1: Set<Var>, set2: Set<Var>): Set<Var> {
   return newSet
 }
 
-function minus(set1: Set<Var>, set2: Set<Var>): Set<Var> {
-  const newSet: Set<Var> = new Set()
+function minus(set1: Set<string>, set2: Set<string>): Set<string> {
+  const newSet: Set<string> = new Set()
 
   for (let item of set1)
     newSet.add(item)
@@ -100,84 +96,22 @@ function minus(set1: Set<Var>, set2: Set<Var>): Set<Var> {
 // FV(MN) = FV(M) union FV(N)
 // FV(lambda x [M]) = FV(M) - {x}
 
-function freeVar(exp: Exp): Set<Var> {
-  if (isVar(exp)) {
-    let t = exp as Var
-    return new Set(t)
+function freeVar(exp: Exp): Set<string> {
+  switch (exp.kind) {
+    case "Var": return new Set(exp.name)
+    case "Ap":  return union(freeVar(exp.target), freeVar(exp.arg))
+    case "Fn":  return minus(freeVar(exp.body), freeVar(exp.var))
   }
-  else if (isAp(exp)) {
-    let ap = exp as Ap
-    return union(freeVar(ap.target), freeVar(ap.arg))
-  }
-  else if (isFn(exp)) {
-    let fn = exp as Fn
-    return minus(freeVar(fn.body), freeVar(fn.var))
-  }
-  throw new Error("Unsupported exp")
 }
 
 // BV(x) = {empty}
 // BV(MN) = BV(M) union BV(N)
 // BV(lambda x [M]) = BV(M) union {x}
 
-function boundVar(exp: Exp): Set<Var> {
-  if (isVar(exp)) {
-    return new Set()
+function boundVar(exp: Exp): Set<string> {
+  switch (exp.kind) {
+    case "Var": return new Set()
+    case "Ap":  return union(boundVar(exp.target), boundVar(exp.arg))
+    case "Fn":  return union(boundVar(exp.body), boundVar(exp.var))
   }
-  else if (isAp(exp)) {
-    let ap = exp as Ap
-    return union(boundVar(ap.target), boundVar(ap.arg))
-  }
-  else if (isFn(exp)) {
-    let fn = exp as Fn
-    return union(boundVar(fn.body), new Set(fn.var))
-  }
-  throw new Error("Unsupported exp")
-}
-
-{
-  // ((lambda (t) (lambda (f) t)) (lambda (x) x))
-  // =>
-  // (lambda (f) (lambda (x) x))
-  const env: Env = new Map()
-  console.dir(
-    evaluate(
-    {target: {var: "t", body: {var: "f", body: "t"}} , arg: {var: "x", body: "x"}},
-    env
-    ),
-    {depth: null}
-  )
-  console.log(freeVar({target: {var: "t", body: {var: "f", body: "t"}} , arg: {var: "x", body: "x"}}))
-  console.log(boundVar({target: {var: "t", body: {var: "f", body: "t"}} , arg: {var: "x", body: "x"}}))
-}
-
-{
-  // ((lambda (t) (lambda (t) t)) (lambda (x) x))
-  // =>
-  // (lambda (t) t)
-  const env: Env = new Map()
-  console.dir(
-    evaluate(
-      {target: {var: "t", body: {var: "t", body: "t"}} , arg: {var: "x", body: "x"}},
-      env
-    ),
-    {depth: null}
-  )
-}
-
-{
-  // ((lambda (t) (t (lambda (t) t))) (lambda (x) x))
-  // =>
-  // ((lambda (x) x) (lambda (t) t))
-  // =>
-  // (lambda (t) t)
-
-  const env: Env = new Map()
-  console.dir(
-    evaluate(
-      {target: {var: "t", body: {target: "t", arg: {var: "t", body: "t"}}} , arg: {var: "x", body: "x"} },
-      env
-    ),
-    {depth: null}
-  )
 }
